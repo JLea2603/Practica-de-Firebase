11af1b7b2bf8f801890cd64337cb6c9a663aded7
Frank5031 <20220332@ricaldone.edu.sv>
2024-07-08 13:55:22 -0600

    Arreglar credenciales de firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,12 +5,12 @@ import { API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, 
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: API_KEY,
-  authDomain: AUTH_DOMAIN,
-  projectId: PROJECT_ID,
-  storageBucket: STORAGE_BUCKET,
-  messagingSenderId: MESSAGING_SENDER_ID,
-  appId: APP_ID    
+  apiKey: "AIzaSyDbbOlbp_RaAgIQhvfCIBrVp3vdHOWLAVY",
+  authDomain: "practica-firebase-20220332.firebaseapp.com",
+  projectId: "practica-firebase-20220332",
+  storageBucket: "practica-firebase-20220332.appspot.com",
+  messagingSenderId: "665205588545",
+  appId: "1:665205588545:web:de3e277b272584e1f93cc0"
 };
 
 console.log("Valor de configuracion", firebaseConfig);
